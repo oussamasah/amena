14707f9f42b3f3f4326952b264c720dc584eb175
osahraoui70 <osahraoui70@gmail.com>
2023-07-14 15:05:06 +0100

    pdf

--- src/Controller/PackageController.php
+++ src/Controller/PackageController.php
@@ -14,21 +14,22 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Mercure\HubInterface;
 use Dompdf\Dompdf;
 use Symfony\Component\Mercure\Update;
+
 class PackageController extends AbstractController
 {
     private $security;
     private $entityManager;
 
-    public function __construct(Security $security,ManagerRegistry $doctrine)
+    public function __construct(Security $security, ManagerRegistry $doctrine)
     {
-        
+
         // Avoid calling getUser() in the constructor: auth may not
         // be complete yet. Instead, store the entire Security object.
         $this->security = $security;
         $this->entityManager = $doctrine->getManager();
         $user = $this->security->getUser();
-        if(!$user){
-            return $this->RedirectToRoute('app_login');    
+        if (!$user) {
+            return $this->RedirectToRoute('app_login');
         }
     }
     #[Route('/package', name: 'app_package')]
@@ -36,34 +37,34 @@ class PackageController extends AbstractController
     {
 
         $user = $this->security->getUser();
-        if(!$user){
-            return $this->RedirectToRoute('app_login');    
+        if (!$user) {
+            return $this->RedirectToRoute('app_login');
         }
-     
+
         if (in_array('EXPEDITOR_ROLE', $user->getRoles(), true)) {
 
-            
+
             $packages = $user->getPackages();
-        
+
             return $this->render('Expeditor/package/index.html.twig', [
                 'packages' => $packages->toArray(),
             ]);
         }
         if (in_array('ADMIN_ROLE', $user->getRoles(), true)) {
 
-          $packages =  $this->entityManager->getRepository(Package::class)->findAllForAdmin();
+            $packages = $this->entityManager->getRepository(Package::class)->findAllForAdmin();
             return $this->render('Admin/package/index.html.twig', [
                 'packages' => $packages,
-            ]); 
+            ]);
         }
-        
+
         if (in_array('DELIVERY_ROLE', $user->getRoles(), true)) {
 
-            $packages =  $this->entityManager->getRepository(Package::class)->findAllForDelivery($user->getId());
-              return $this->render('Delivery/package/index.html.twig', [
-                  'packages' => $packages,
-              ]); 
-          }
+            $packages = $this->entityManager->getRepository(Package::class)->findAllForDelivery($user->getId());
+            return $this->render('Delivery/package/index.html.twig', [
+                'packages' => $packages,
+            ]);
+        }
 
 
     }
@@ -74,131 +75,127 @@ class PackageController extends AbstractController
         $user = $this->security->getUser();
         $pack = new Package();
 
-        $form = $this->createForm(PackageType::class,$pack);
+        $form = $this->createForm(PackageType::class, $pack);
         $form->handlerequest($req);
-        if($form->isSubmitted()  && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $pack->setState("waiting");
-            $date = new \DateTime('@'.strtotime('now'));
-        
-            $pack->setCreateDate( $date);
-            $pack->setExpeditor( $user);
+            $date = new \DateTime('@' . strtotime('now'));
+
+            $pack->setCreateDate($date);
+            $pack->setExpeditor($user);
             $this->entityManager->persist($pack);
             $this->entityManager->flush();
-          return  $this->redirectToRoute('app_package',array('msg' => "Package added successfuly"));
+            return $this->redirectToRoute('app_package', array('msg' => "Package added successfuly"));
         }
         return $this->render('expeditor/package/add.html.twig', [
             'form' => $form->createView(),
-        ]); 
+        ]);
 
 
     }
 
 
     #[Route('/package/validate/{id}', name: 'app_validate_package')]
-    public function validate(Request $req,$id,HubInterface $hub): Response
+    public function validate(Request $req, $id, HubInterface $hub): Response
     {
-       $pack = $this->entityManager->getRepository(Package::class)->find($id);
-       if($pack){
-        $pack->setState("approved");
-        $this->entityManager->persist($pack);
-        $this->entityManager->flush();
-        $update = new Update(
-            'https://example.com/books/1',
-            json_encode(['message' => 'New package has been approved ID : '.$id])
-        );
+        $pack = $this->entityManager->getRepository(Package::class)->find($id);
+        if ($pack) {
+            $pack->setState("approved");
+            $this->entityManager->persist($pack);
+            $this->entityManager->flush();
 
-        $hub->publish($update);
-        return $this->redirectToRoute("app_package",["msg"=>"Your package approved successfuly"]);
-       }else{
-        return $this->redirectToRoute("app_package",["error"=>"The package is not disponible"]);
-       }
-        
+            return $this->redirectToRoute("app_package", ["msg" => "Your package approved successfuly"]);
+        } else {
+            return $this->redirectToRoute("app_package", ["error" => "The package is not disponible"]);
+        }
+
     }
     #[Route('/package/picked/{id}', name: 'app_picked_package')]
-    public function picked(Request $req,$id): Response
+    public function picked(Request $req, $id): Response
     {
-       $pack = $this->entityManager->getRepository(Package::class)->find($id);
-       if($pack){
-        $pack->setState("picked");
-        $this->entityManager->persist($pack);
-        $this->entityManager->flush();
-        return $this->redirectToRoute("app_package",["msg"=>"Your package approved successfuly"]);
-       }else{
-        return $this->redirectToRoute("app_package",["error"=>"The package is not disponible"]);
-       }
-        
+        $pack = $this->entityManager->getRepository(Package::class)->find($id);
+        if ($pack) {
+            $pack->setState("picked");
+            $this->entityManager->persist($pack);
+            $this->entityManager->flush();
+            return $this->redirectToRoute("app_package", ["msg" => "Your package approved successfuly"]);
+        } else {
+            return $this->redirectToRoute("app_package", ["error" => "The package is not disponible"]);
+        }
+
     }
 
     #[Route('/package/edit/{id}', name: 'app_edit_package')]
-    public function edit(Request $req,$id): Response
+    public function edit(Request $req, $id): Response
     {
-       $pack = $this->entityManager->getRepository(Package::class)->find($id);
-   
-       $form = $this->createForm(PackageType::class,$pack);
-       $form->handlerequest($req);
-       if($form->isSubmitted()  && $form->isValid()){
-        $p = new Package();
-        $p = $form->getData();
-         $this->entityManager->getRepository(Package::class)->save($p,true);
-         
-         return  $this->redirectToRoute('app_package',array('msg' => "Package edited successfuly"));
-       }
-       return $this->render('expeditor/package/edit.html.twig', [
-           'form' => $form->createView(),
-       ]); 
-        
+        $pack = $this->entityManager->getRepository(Package::class)->find($id);
+
+        $form = $this->createForm(PackageType::class, $pack);
+        $form->handlerequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $p = new Package();
+            $p = $form->getData();
+            $this->entityManager->getRepository(Package::class)->save($p, true);
+
+            return $this->redirectToRoute('app_package', array('msg' => "Package edited successfuly"));
+        }
+        return $this->render('expeditor/package/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
     }
     #[Route('/package/delivery/{id}', name: 'app_delivery_package')]
-    public function delivery(Request $req,$id): Response
+    public function delivery(Request $req, $id): Response
     {
-       $pack = $this->entityManager->getRepository(Package::class)->find($id);
-       $delivery = $this->entityManager->getRepository(User::class)->findByRole("DELIVERY_ROLE");
-   
-       $form = $this->createForm(PackageType::class,$pack);
-       $form->handlerequest($req);
-       if($form->isSubmitted()  && $form->isValid()){
-        $p = new Package();
-        $p = $form->getData();
-         $this->entityManager->getRepository(Package::class)->save($p,true);
-         
-         return  $this->redirectToRoute('app_package',array('msg' => "Package edited successfuly"));
-       }
-       return $this->render('admin/package/edit.html.twig', [
-           'form' => $form->createView(),
-           "delivery"=>$delivery
-       ]); 
-        
+        $pack = $this->entityManager->getRepository(Package::class)->find($id);
+        $delivery = $this->entityManager->getRepository(User::class)->findByRole("DELIVERY_ROLE");
+
+        $form = $this->createForm(PackageType::class, $pack);
+        $form->handlerequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $p = new Package();
+            $p = $form->getData();
+            $this->entityManager->getRepository(Package::class)->save($p, true);
+
+            return $this->redirectToRoute('app_package', array('msg' => "Package edited successfuly"));
+        }
+        return $this->render('admin/package/edit.html.twig', [
+            'form' => $form->createView(),
+            "delivery" => $delivery
+        ]);
+
     }
     #[Route('/package/pdf/{id}', name: 'app_pdf_package')]
-    public function pdf(Request $req,$id):Response
+    public function pdf(Request $req, $id): Response
     {
-        
-        
+
+
         $html = $this->renderView('pdf/package.html.twig', [
             'specifications' => "",
         ]);
         $dompdf = new Dompdf();
+
         $dompdf->load_html($html);
         $dompdf->setPaper('A4', 'landscape');
-        $dompdf->render(); 
+        $dompdf->render();
         $dompdf->stream("sample.pdf");
-       
+
         return new Response('', 200, [
             'Content-Type' => 'application/pdf',
-          ]);
-        
+        ]);
+
     }
     #[Route('/package/delete/{id}', name: 'app_delete_package')]
-    public function delete(Request $req,$id): Response
+    public function delete(Request $req, $id): Response
     {
-       $pack = $this->entityManager->getRepository(Package::class)->find($id);
-       if($pack){
-        $this->entityManager->remove($pack);
-        $this->entityManager->flush();
-        return $this->redirectToRoute("app_package",["msg"=>"Your package deleted successfuly"]);
-       }else{
-        return $this->redirectToRoute("app_package",["error"=>"The package is not disponible"]);
-       }
-        
+        $pack = $this->entityManager->getRepository(Package::class)->find($id);
+        if ($pack) {
+            $this->entityManager->remove($pack);
+            $this->entityManager->flush();
+            return $this->redirectToRoute("app_package", ["msg" => "Your package deleted successfuly"]);
+        } else {
+            return $this->redirectToRoute("app_package", ["error" => "The package is not disponible"]);
+        }
+
     }
 }
